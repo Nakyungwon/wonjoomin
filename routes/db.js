d966e33533e09c58e4f4978aa5a937e4c6209ff5
Nakyugnwon <saecomaster@naver.com>
2018-02-06 02:37:53 +0900

    dbdbdbdbdb

---
+++ routes/db.js
@@ -0,0 +1,17 @@
+const {Pool} = require('pg');
+
+
+module.exports = {
+  query: async function(text, params, callback)  {
+    console.log(text);
+    console.log(params);
+    let result = '';
+      const client = await pool.connect();
+      try{
+        result = await client.query(text, params)
+      }finally{
+        client.release();
+        return result;
+      }
+  }
+}
