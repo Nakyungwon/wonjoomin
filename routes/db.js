84ad901918bfeb032df27757ff33095983b76fa9
Nakyugnwon <saecomaster@naver.com>
2018-02-06 02:24:42 +0900

    디비삭제

--- routes/db.js
+++ routes/db.js
@@ -1,22 +1,5 @@
 const {Pool} = require('pg');
 
-/* DB 정보 */ 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database:'postgres',
-    password:'sksmssk',
-    port: 5432 ,
-    max: 20
-});
-
-// const pool = new Pool({
-//   user: 'saecomaster',
-//   host: 'onejumin.ciojezwttlpc.ap-northeast-2.rds.amazonaws.com',
-//   database:'postgres',
-//   password:'sksmssk12!',
-//   port: 5432 ,
-// });
 
 module.exports = {
   query: async function(text, params, callback)  {
